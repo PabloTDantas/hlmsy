fff0e086ec9b3940dee1ffcc34d47ce1adeb0108
Holmes <89360392+PabloTDantas@users.noreply.github.com>
2023-05-13 13:42:54 -0300

    Update configuração.js

--- configuração.js
+++ configuração.js
@@ -4,8 +4,8 @@ module.exports = {
     port: '50',
     apikeypremium: 'Yumepv',
     limitCount: 9999999999999999999999999999999999999999,
-    dinheiroCount: 1,
-    limitPremium: 99999999,
+    dinheiroCount: 9999999999999999999999999999999999999999999999999999999999999999999999999,
+    limitPremium: 99999999999999999999999999999999999999999999999999999999999999999999,
     tokens: 'Yubi_ofc_adm', 
     dbURI: `mongodb+srv://robotsmr:8AxLK8l0FXHI1zac@cluster0.awlbsnt.mongodb.net/?retryWrites=true&w=majority`
 };
