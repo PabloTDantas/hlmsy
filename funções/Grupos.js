05de4a46efd64533b0a89fb3fe357664b6a01f61
Holmes <89360392+PabloTDantas@users.noreply.github.com>
2023-05-13 12:18:27 -0300

    Update Grupos.js

--- funções/Grupos.js
+++ funções/Grupos.js
@@ -68,7 +68,7 @@ const tgApis = [
 	{
 		apiId: 24216953, // coloque apild aqui 
 		apiHash: "0512d08ef5adbaa122eee0a00ad9242a ", // coloque a apiHash aqui 
-		stringSession: "1AQAOMTQ5LjE1NC4xNzUuNTQBu3AIsirVZA9mk1PI7efyOohq00n7OWVSfsvT5xepiB7a/c6Rr/T3jfqOperjRVo6GdR2ydL22EsEuVksbVed7oXJfnJJwg5Nkl6YwfzHgmzHLFcik9CZcxA6c/KAH52RpULomumSRzdudWHDKi2vAYYBHuS6kv+6nP21L2xxyU1NuG2PByn+QL3Dgymq5kvpFoWxCyFkIe6SGoAIeyiFzL/N8S7ZHKYGYmsj9KS54iYzgsC3IWDGmnI0VIrocq+uUmJCFI2fMzP0y7fqIUftwOVYPytHAa3XpAnCWWSgk8WlUTGg2dskLivpuvPwkQzutlt5/tHO5k8T18VK7d574A4=", // coloque a string aqui 
+		stringSession: "1AQAOMTQ5LjE1NC4xNzUuNTQBuz3RPuhzj8B90LlCq1j0A/emZy470JjLSWR7Rojyx9utZiZ1ZXEG0en5v3Ibs93nluUIPH3mDiVlWbr2sSdasmKW/RowZIm7UEwQAYdCoDY5x/23tSuOzhmvlp+YRY8scgecJZY5h2rbvgZLFLcbGX9H6tpbHgBXgjo2/oYwG0bK8nifWxYtufq3Hh/HpQgnIxFRP4IyJiRze4BAsvHuYHez5ncb89fYcgCQ6LSGHb1tJCPpOTm5EyL9LGiK0eViSe7VyR0JGvZrbcAQ+LXye9ybt4RtXWGjgOnLUVHhwcC384oTJdLeA0G/ypT7lhEaSyM6Fufo4Jth1vCGDrK0WtQ=", // coloque a string aqui 
 		number: "+5561986757412", // coloque o número do telegram aqui 
 	},
 ];
